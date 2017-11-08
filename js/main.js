5c5cc88d1df41832b65b5daa9f2cf6f48d1e39b8
ethanblumenthal <ethan.blumenthal@gamil.com>
2017-11-08 10:48:44 -0800

    added functions to main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardsInPlay[0]);
-console.log('User flipped ' + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert('You found a match!');
   } else {
     alert('Sorry, try again.');
   }
 }
+
+function flipCard(cardID) {
+  console.log('User flipped ' + cards[cardID]);
+  cardsInPlay.push(cards[cardID]);
+
+  if (cardsInPlay.length === 2) {
+    checkForMatch();
+  }
+}
+
+flipCard(0);
+flipCard(2);
